bc3e3b90be8a2145bff76ee09607dd7ebb76e789
J-eyJ <60826745+J-D-J123@users.noreply.github.com>
2024-11-02 01:20:36 -0500

    added main Game file

---
+++ Game.py
@@ -0,0 +1,15 @@
+# Author:   J-D-J123
+# Date:     10/2/2024
+# File:     Game.py
+# desc:     main game file this will controll all the game logic 
+
+class Game:
+
+    # main call from start.py to call the methods 
+    def __init__(self):
+        print("call to Game() has worked!")
+        self.setUpGame()
+    
+
+    def setUpGame(self):
+        print("making game display")

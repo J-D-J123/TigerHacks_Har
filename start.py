7fc8351661dff59ddd768b5de11359ee843ad709
J-eyJ <60826745+J-D-J123@users.noreply.github.com>
2024-11-01 20:13:44 -0500

    menu update

--- start.py
+++ start.py
@@ -1,10 +1,45 @@
 # start.py 
 # this starts the main menu
 
+import tkinter as tk
+from PIL import Image, ImageTk 
+
 def makeMenu():
-    # TODO make this method 
-    pass
+
+    WIDTH   = 300
+    HEIGHT  = 200
+
+    print("making menu")
+
+    # set up the window 
+    root = tk.Tk()
+    root.geometry(f"{WIDTH}x{HEIGHT}")
+    # root.configure(bg="black")
+
+    # load bg image
+    orginalImg   = Image.open("assets/pictures/menuBg.png")
+    resizedImg   = resizeImage(orginalImg, WIDTH, HEIGHT)
+    photoBgImage = ImageTk.PhotoImage(resizedImg)
+
+    label = tk.Label(root, image=photoBgImage)
+    label.place(relwidth=1, relheight=1)
+
+    # create start button 
+    startButton = tk.Button(root, text="Start")
+    startButton.grid(row=0, column=0, padx=20, pady=20)
+
+    # center the buttons
+    root.grid_rowconfigure(0, weight=1)
+    root.grid_columnconfigure(0,weight=1)
+
+    root.mainloop()
+
+def resizeImage(image, width, height):
+    return image.resize((width, height), Image.LANCZOS) 
 
 def main(): 
     # game loop logic
     makeMenu()
+
+if __name__ == "__main__":
+    main()

604da64dc908b2289a96a3add174dc452a87081a
mlarkin26 <michael.larkin11.16@gmail.com>
2024-11-03 09:32:01 -0600

    Changing button colors

--- start.py
+++ start.py
@@ -21,9 +21,9 @@ pygame.display.set_caption("Truman's Farm Harvester")
 
 # Colors
 WHITE           = (255, 255, 255)
-RED             = (255, 0, 0)
+RED             = (255, 88, 116)
 SAND            = (194, 178, 128)
-GREEN           = (0, 255, 0)
+GREEN           = (88, 255, 116)
 GREY            = (200, 200, 200)
 BLACK           = (0, 0, 0)
 
